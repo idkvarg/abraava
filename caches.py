d6d9aa2d64954a215ffe3b20ed47806c17a57afb
Hamidreza Ahmadi <idkvarg@outlook.com>
2025-09-08 19:38:18 +0330

    ..

--- caches.py
+++
@@ -1,8 +0,0 @@
-# caches.py
-from cachetools import TTLCache
-
-from config import SEARCH_CACHE_TTL, DOWNLOAD_CACHE_TTL
-
-# Caches with TTL
-SEARCH_CACHE = TTLCache(maxsize=500, ttl=SEARCH_CACHE_TTL)
-DOWNLOAD_CACHE = TTLCache(maxsize=500, ttl=DOWNLOAD_CACHE_TTL)
